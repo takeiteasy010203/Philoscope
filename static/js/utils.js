03b6c211d42383a87296a7b90aebab9160842ea4
Hieu Nguyen Dinh <hieunguyen5a4@gmail.com>
2026-07-26 19:05:21 +0700

    Update utils.js

--- static/js/utils.js
+++ static/js/utils.js
@@ -35,15 +35,41 @@ const json_qdata = result.json_qdata || {};
 
         // In response ra màn hình
 responseArea.innerHTML = `
-            <div class="px-2 border border-3 border-primary rounded"> 
-        <div class="text-center">
-            <span class="display-5  fst-italic  fw-semibold" >ANALYSIS📜</span>
-        </div>
-
+            <div class="px-2 "> 
         <div class="plot-container">
-            
             <div id="content_block" class="d-flex align-items-start flex-column summary-box plot-wrapper">
-                 
+                <div class="text_block_1 shadow-hover shadow p-3 row rounded-3 mb-3 ">
+                    <img src="/media/Team work-rafiki.svg" class="col-md-4 text_pic" alt="">
+                    <div class="inside_content col-md-8">
+                        <div class="text_label_1 text_label align-items-center lh-lg"></div>
+                        <div class="text_evidence_1 text_evidence "></div>
+                    </div>
+                </div>
+
+                <div class="text_block_2 shadow-hover shadow p-3 row rounded-3 mb-3 ">
+                    <img src="/media/Design thinking-rafiki.svg" class="col-md-4 text_pic" alt="">
+                    <div class="inside_content col-md-8">
+                        <div class="text_label_2 text_label align-items-center lh-lg"></div>
+                        <div class="text_evidence_2 text_evidence "></div>
+                    </div>
+                </div>
+
+                <div class="text_block_3 shadow-hover shadow p-3 row rounded-3 mb-3 ">
+                    <img src="/media/Solar system-rafiki.svg" class="col-md-4 text_pic" alt="">
+                    <div class="inside_content col-md-8">
+                        <div class="text_label_3 text_label align-items-center lh-lg"></div>
+                        <div class="text_evidence_3 text_evidence "></div>
+                    </div>
+                </div>
+
+                <div class="text_block_4 shadow-hover shadow p-3 row rounded-3 mb-3 ">
+                    <img src="/media/Choose-rafiki.svg" class="col-md-4 text_pic" alt="">
+                    <div class="inside_content col-md-8">
+                        <div class="text_label_4 text_label align-items-center lh-lg"></div>
+                        <div class="text_evidence_4 text_evidence "></div>
+                    </div>
+                </div>
+               
                
             </div>
             <div id="chart-box" class="chart-box  justify-content-center align-items-center my-3">
@@ -69,17 +95,31 @@ responseArea.innerHTML = `
     </div>
         `;
 const content_block = document.getElementById('content_block')
-    
+//1
+const text_label_1 = content_block.querySelector('.text_label_1');
+const text_evidence_1 = content_block.querySelector('.text_evidence_1');
+//2
+const text_label_2 = content_block.querySelector('.text_label_2');
+const text_evidence_2 = content_block.querySelector('.text_evidence_2');
+//3
+const text_label_3 = content_block.querySelector('.text_label_3');
+const text_evidence_3 = content_block.querySelector('.text_evidence_3');
+//4
+const text_label_4 = content_block.querySelector('.text_label_4');
+const text_evidence_4 = content_block.querySelector('.text_evidence_4');
     Object.entries(json_qdata).forEach(
         ([mainKey, mainValue]) => {
-    const label = document.createElement('div')
-    label.classList.add('text_label')
-    label.textContent = mainValue['label']
-    const evidence = document.createElement('div')
-    evidence.classList.add('text_evidence')
-    evidence.textContent = mainValue['evidence'] || ''
-    content_block.append(label)
-    content_block.append(evidence)
+            text_label_1.textContent = json_qdata["individualism_collectivism"].label;
+            text_evidence_1.textContent = json_qdata["individualism_collectivism"].evidence;
+           
+            text_label_2.textContent = json_qdata["rationalism_irrationalism"].label;
+            text_evidence_2.textContent = json_qdata["rationalism_irrationalism"].evidence;
+           
+            text_label_3.textContent = json_qdata["universalism_relativism"].label;
+            text_evidence_3.textContent = json_qdata["universalism_relativism"].evidence;
+           
+            text_label_4.textContent = json_qdata["determinism_free_will"].label;
+            text_evidence_4.textContent = json_qdata["determinism_free_will"].evidence;
     }
 )
 const graphData = JSON.parse(result.graph_data);
